9ee8dcb71056de69fe38c99e80e51e3e69fcf88f
Nicoli <nicolioliveira10@yahoo.com.br>
2023-01-06 14:09:29 -0300

    conexão com o banco msql criada

--- product/src/main.js
+++ product/src/main.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { router } from './routes.js';
-
 import docs from 'swagger-ui-express'; 
 import apidocs from '../api-docs.json' assert{type:'json'};
 
+
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(router);
 app.use("/docs", docs.serve,docs.setup(apidocs));
 
